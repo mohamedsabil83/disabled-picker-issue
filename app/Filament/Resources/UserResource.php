76564ef93d3be9e5c0562a1782ae261b1d5a2bd6
mohamedsabil83 <me@mohamedsabil83.com>
2022-09-26 18:34:54 +0300

    Issue example

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make([
+                    Forms\Components\Toggle::make('is_active1')
+                        ->reactive(),
+                    Forms\Components\DatePicker::make('date1')
+                        ->disabled(fn (callable $get) => false === $get('is_active1')),
+                ]),
+
+                Forms\Components\Card::make([
+                    Forms\Components\Toggle::make('is_active2')
+                        ->reactive()
+                        ->default(true),
+                    Forms\Components\DatePicker::make('date2')
+                        ->disabled(fn (callable $get) => false === $get('is_active2')),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
